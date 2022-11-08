6a25806181d9f8390172b73bc46d5de76393ff0a
Andrew Lock <andrewlock.net@gmail.com>
2022-11-08 19:26:25 +0000

    Add Chapter 27

---
+++ Chapter27/A_RecipeApplication_SQLite/RecipeApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RecipeApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RecipeService _service;
+        private readonly ILogger<IndexModel> _logger;
+        public ICollection<RecipeSummaryViewModel> Recipes { get; private set; }
+
+        public IndexModel(RecipeService service, ILogger<IndexModel> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            Recipes = await _service.GetRecipes();
+            _logger.LogInformation("Loaded {RecipeCount} recipes", Recipes.Count);
+        }
+    }
+}
