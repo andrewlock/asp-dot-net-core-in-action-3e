9ca364a7bb127199068ad56698a159dec566f602
Andrew Lock <andrewlock.net@gmail.com>
2023-03-17 20:58:16 +0000

    Improve route group example to compare with no route groups

--- Chapter05/H_MinimalApiRouteGroups/MinimalApiRouteGroups/Program.cs
+++ Chapter05/H_MinimalApiRouteGroups/MinimalApiRouteGroups/Program.cs
@@ -6,39 +6,87 @@ WebApplication app = builder.Build();
 
 var _fruit = new ConcurrentDictionary<string, Fruit>();
 
-RouteGroupBuilder fruitApi = app.MapGroup("/fruit");
-
-fruitApi.MapGet("/", () => _fruit);
-
-RouteGroupBuilder fruitApiWithValidation = fruitApi.MapGroup("/");
-    //.AddFilter(ValidationHelper.ValidateIdFactory);
-
-fruitApiWithValidation.MapGet("/{id}", (string id) =>
-    _fruit.TryGetValue(id, out var fruit)
-        ? TypedResults.Ok(fruit)
-        : Results.Problem(statusCode: 404));
-
-fruitApiWithValidation.MapPost("/{id}", (Fruit fruit, string id) =>
-    _fruit.TryAdd(id, fruit)
-        ? TypedResults.Created($"/fruit/{id}", fruit)
-        : Results.ValidationProblem(new Dictionary<string, string[]>
-          {
-              { "id", new[] { "A fruit with this id already exists" } }
-        }));
-
-fruitApiWithValidation.MapPut("/{id}", (string id, Fruit fruit) =>
-{
-    _fruit[id] = fruit;
-    return Results.NoContent();
-});
-
-fruitApiWithValidation.MapDelete("/fruit/{id}", (string id) =>
-{
-    _fruit.TryRemove(id, out _);
-    return Results.NoContent();
-});
+// WithoutRouteGroups();
+UsingRouteGroups();
 
 app.Run();
+
+void WithoutRouteGroups()
+{
+    // Same /fruit prefix 👇
+    app.MapGet("/fruit", () => _fruit);
+
+    // Same /fruit prefix 👇
+    app.MapGet("/fruit/{id}", (string id) =>
+        _fruit.TryGetValue(id, out var fruit)
+            ? TypedResults.Ok(fruit)
+            : Results.Problem(statusCode: 404))
+        .AddEndpointFilterFactory(ValidationHelper.ValidateIdFactory); // 👈 Duplicated on every endpoint
+
+    // Same /fruit prefix 👇
+    app.MapPost("/fruit/{id}", (Fruit fruit, string id) =>
+        _fruit.TryAdd(id, fruit)
+            ? TypedResults.Created($"/fruit/{id}", fruit)
+            : Results.ValidationProblem(new Dictionary<string, string[]>
+            {
+                { "id", new[] { "A fruit with this id already exists" } }
+            }))
+        .AddEndpointFilterFactory(ValidationHelper.ValidateIdFactory); // 👈 Duplicated on every endpoint
+
+    // Same /fruit prefix 👇
+    app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+    {
+        _fruit[id] = fruit;
+        return Results.NoContent();
+    })
+        .AddEndpointFilterFactory(ValidationHelper.ValidateIdFactory); // 👈 Duplicated on every endpoint
+
+    // Same /fruit prefix 👇
+    app.MapDelete("/fruit/{id}", (string id) =>
+    {
+        _fruit.TryRemove(id, out _);
+        return Results.NoContent();
+    })
+        .AddEndpointFilterFactory(ValidationHelper.ValidateIdFactory); // 👈 Duplicated on every endpoint
+}
+
+void UsingRouteGroups()
+{
+    // Fruit prefix only defined in one place        👇
+    RouteGroupBuilder fruitApi = app.MapGroup("/fruit");
+
+    fruitApi.MapGet("/", () => _fruit);
+
+    // Nested group. "/" prefix means still only using "/fruit" prefix 👇
+    RouteGroupBuilder fruitApiWithValidation = fruitApi.MapGroup("/")
+        .AddEndpointFilterFactory(ValidationHelper.ValidateIdFactory); // 👈 Validation filter only declared once
+
+    fruitApiWithValidation.MapGet("/{id}", (string id) =>
+        _fruit.TryGetValue(id, out var fruit)
+            ? TypedResults.Ok(fruit)
+            : Results.Problem(statusCode: 404));
+
+    fruitApiWithValidation.MapPost("/{id}", (Fruit fruit, string id) =>
+        _fruit.TryAdd(id, fruit)
+            ? TypedResults.Created($"/fruit/{id}", fruit)
+            : Results.ValidationProblem(new Dictionary<string, string[]>
+            {
+                { "id", new[] { "A fruit with this id already exists" } }
+            }));
+
+    fruitApiWithValidation.MapPut("/{id}", (string id, Fruit fruit) =>
+    {
+        _fruit[id] = fruit;
+        return Results.NoContent();
+    });
+
+    fruitApiWithValidation.MapDelete("/{id}", (string id) =>
+    {
+        _fruit.TryRemove(id, out _);
+        return Results.NoContent();
+    });
+}
+
 record Fruit(string Name, int Stock);
 class ValidationHelper
 {
