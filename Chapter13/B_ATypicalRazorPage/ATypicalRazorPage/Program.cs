b6625a548d1a2c694e0584775bf10a6591ed013b
Andrew Lock <andrew.lock@datadoghq.com>
2023-08-01 12:26:09 +0100

    Add fix for missing redirect in Chapter 13

--- Chapter13/B_ATypicalRazorPage/ATypicalRazorPage/Program.cs
+++ Chapter13/B_ATypicalRazorPage/ATypicalRazorPage/Program.cs
@@ -10,4 +10,8 @@ var app = builder.Build();
 
 app.MapRazorPages();
 
+// When viewing in the browser, the default page ('/') is requested
+// But as that page does not exist, redirect to a page that does!
+// You can also try /category/Long 
+app.Map("/", () => Results.Redirect("/category/Simple"));
 app.Run();
